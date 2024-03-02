04df9db89ed5f4ee59a8bb6d0b8053d20c36f632
sofia-caballero <160689113+sofia-caballero@users.noreply.github.com>
2024-03-01 22:37:01 -0500

    Add files via upload

---
+++ michel/6cilindro.js
@@ -0,0 +1,19 @@
+// Entradas
+
+console.log("6. Escribir un algoritmo que determine el área y el volúmen de un cilindro");
+
+console.log("Ingrese el radio sel cilindro ");
+let radio= 4.5;
+
+console.log("Ingrese la altura sel cilindro ");
+let altura = 10.5;
+
+
+
+//proceso
+let area = (3.1416*radio*radio) ;
+let volumen =(area*altura);
+
+//salida
+console.log(area);
+console.log(volumen);
---
+++ michel/7radio.js
@@ -0,0 +1,16 @@
+// Entradas
+
+console.log("7. Realizar un algoritmo que lea el radio de una circunferencia y escriba la longitud de la misma y el área  del círculo inscrito");
+
+console.log("Ingrese el radio de la circunferencia");
+let radio= 5;
+
+
+
+//proceso
+let longitud = (2*3.1416*radio) ;
+let area =Math.PI * Math.pow(radio, 2);
+
+//salida
+console.log(longitud);
+console.log(area);
